3ae58d9de2d6e9fae1ce703c4f4b868213d22dd4
jaylamien28 <hengmien060728@gmail.com>
2025-09-02 22:08:17 +0800

    Update login_module.cpp

--- login_module.cpp
+++ login_module.cpp
@@ -1,12 +1,3 @@
-#include <iostream>
-#include <iomanip>
-#include <string>
-#include <vector>
-#include <fstream>
-#include <algorithm>
-#include <regex>
-using namespace std;
-
 struct PersonalInfo{
 	string fullName;
 	string email;
